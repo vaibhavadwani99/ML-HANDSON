38cc4a294e237996e86d95118761800d8a80a7de
vaibhavadwani <vaibhav.adwani@tigeranalytics.com>
2022-03-07 19:55:43 +0530

    modified load_housing_data function

--- nonstandardcode.py
+++ nonstandardcode.py
@@ -22,12 +22,11 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 import pandas as pd
 
 def load_housing_data(housing_path=HOUSING_PATH):
-    fetch_housing_data()
-    housing= load_housing_data()
+    
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
-
-housing = load_housing_data
+fetch_housing_data()
+housing = load_housing_data()
 
 from sklearn.model_selection import train_test_split
 
