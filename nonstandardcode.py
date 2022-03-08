809a6d87c2aa06a60fc954419b3376f2b3d5c505
vaibhavadwani <vaibhav.adwani@tigeranalytics.com>
2022-03-08 10:28:49 +0530

    call_housing_value_function

--- nonstandardcode.py
+++ nonstandardcode.py
@@ -25,6 +25,8 @@ def load_housing_data(housing_path=HOUSING_PATH):
     
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
+
+
 fetch_housing_data()
 housing = load_housing_data()
 
