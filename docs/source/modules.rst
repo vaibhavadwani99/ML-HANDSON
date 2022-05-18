aa9f43649e89913405f9b16763b4aecbe2f2afc8
vaibhavadwani <vaibhav.adwani@tigeranalytics.com>
2022-05-18 20:23:28 +0530

    create separate folder for project

--- docs/source/modules.rst
+++
@@ -1,7 +0,0 @@
-housing_price_prediction
-========================
-
-.. toctree::
-   :maxdepth: 4
-
-   housing_price_prediction
--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name = 'housing_price_prediction',
-    version = 0.2,
-    packages = find_packages(where="src"),
-    package_dir = {"":"src"}
-)
